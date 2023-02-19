a1bcb9f6f6d71c5b2dbf81fc0058c6e509d921f3
HelgaToshkollari <toshkollari.helga@gmail.com>
2023-02-19 20:43:12 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default{
+  data(){
+    return{
+      "text": "hello"
+    }
+  }
+
+}
+</script>
+
+<template>
+ <h1>{{ text }}</h1>
+</template>
+
+<style lang="scss">
+@use "styles/main.scss";
+
+</style>

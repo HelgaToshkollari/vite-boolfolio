b38ca3d2f384de2a98bf0893c3f4a8d1df40bd77
HelgaToshkollari <toshkollari.helga@gmail.com>
2023-02-19 22:04:34 +0100

    adds axios

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,61 @@
 <script>
+import TheHeader from "./components/TheHeader.vue";
+
 export default{
+  components:{TheHeader},
   data(){
     return{
-      "text": "hello"
+      projects: []
     }
+  },
+  methods: {
+    fetchProjects(){
+      axios.get("http://127.0.0.1:8000/api/projects").then((resp)=>{
+        console.log(resp.data);
+
+      })
+    
+    } 
+  }, 
+  mounted(){
+    this.fetchProjects();
   }
 
 }
 </script>
 
 <template>
- <h1>{{ text }}</h1>
+  <TheHeader></TheHeader>
+ 
+ <div class="container">
+    <div class="row justify-content-center py-4">
+          <div class="col-md-9">
+            <div class="card">
+            <div class="card-body">
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th>Cover</th>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                <tr v-for="project in projects" :key="project.id">
+                  <td>{{ project.cover_img }}</td>
+                  <td>{{ project.title }}</td>
+                  <td>{{ project.description }}</td>
+                  <td>{{ project.technologies }}</td>
+                </tr>
+                 
+                </tbody>
+              </table> 
+            </div>
+            </div>
+          </div>
+    </div> 
+ </div>
 </template>
 
 <style lang="scss">
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light px-4">
+        
+
+            <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+                <ul class="navbar-nav d-flex">
+                    <li class="nav-item active" v-for="(link, index) in menuLinks" :key="index">
+
+                      <a class="nav-link" :href="link.link">{{ link.label }}</a>
+                    </li>
+                    
+                </ul>
+                <form class="form-inline my-2 my-lg-0 d-flex">
+                  <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+    </div>
+</template>
+<script>
+export default{
+  
+    data(){  
+        return{
+            menuLinks: [
+            {
+                label:"Home",
+                routeName:"home"
+            },
+            {
+                label:"Projects",
+                routeName:"projects"
+            },
+            {
+                label:"About us",
+                routeName:"about"
+            },
+            {
+                label:"Contacts",
+                routeName:"contacts"
+            },
+            ]
+        }
+
+    }
+}
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
